b25984418f88840f1d3acde8402b4969e687f89b
Claude Agnes <rlawnstjddl12@gmail.com>
2022-10-24 17:03:42 +0900

    Upload Files
    Upload Files

---
+++ kakaoMention/kakaoMention/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace kakaoMention
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.human_count = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // human_count
+            // 
+            this.human_count.Location = new System.Drawing.Point(12, 34);
+            this.human_count.Name = "human_count";
+            this.human_count.Size = new System.Drawing.Size(179, 25);
+            this.human_count.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(197, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "채팅방 인원";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(207, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "` 키를누르면 모두 멘션합니다";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 62);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(417, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "꼭 텍스트 박스에 숫자만 입력하고 숫자 입력후에 시작하세요";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(435, 118);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.human_count);
+            this.Name = "Form1";
+            this.Text = "카카오 모두멘션";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox human_count;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ kakaoMention/kakaoMention/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Threading;
+
+namespace kakaoMention
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool isRunning = true;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread th = new Thread(Keyboardd);
+            th.SetApartmentState(ApartmentState.STA);
+            CheckForIllegalCrossThreadCalls = false;
+            th.Start();
+            
+        }
+        
+        void Keyboardd()
+        {
+            int a = 1;
+            int c = 0;
+            while (isRunning)
+            {
+                Thread.Sleep(40);
+                if((Keyboard.GetKeyStates(Key.OemTilde) & KeyStates.Down) > 0)
+                {
+                    SendKeys.SendWait("{BACKSPACE}");
+                    for(int i=0; i<(int.Parse(human_count.Text)-1); i++)
+                    {
+                        SendKeys.SendWait("@");
+                        Thread.Sleep(300);
+                        for(int j=0; j<a; j++)
+                        {
+                            SendKeys.SendWait("{DOWN}");
+                        }
+                        SendKeys.SendWait("{ENTER}");
+                        Thread.Sleep(100);
+                        a++;
+                        c++;
+                        if (c == 15)
+                        { 
+                            SendKeys.SendWait("{ENTER}");
+                            c = 0;
+                        }
+                    }
+                    SendKeys.SendWait("{ENTER}");
+                    Environment.Exit(0);
+                }
+            }
+        }
+    }
+}
